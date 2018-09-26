40f08abf47a12833a3bba450ef8a8043d083ec60
amogchandrashekar <amogmc3@gmail.com>
2018-09-26 17:09:24 +0530

    Create change.py

---
+++ Algorithmic Toolbox/week3_greedy_algorithms/change.py
@@ -0,0 +1,24 @@
+import sys
+
+def get_change(m):
+    '''
+    Greedy implementation of rendering the change.
+    This function returns the minimum number of coins needed to change the input value (an integer) into coins with denominations 1, 5, and 10.
+
+    :param m: input value (an integer).
+    :return: minimum number of coins needed to change the input value (an integer) into coins with denominations 1, 5, and 10.
+
+    example :
+
+    >>> m = 28
+    6
+    explaination : 28 = 10 + 10 + 5 + 1 + 1 + 1.
+    '''
+    denomination_tens = m // 10
+    denomination_five = (m-(denomination_tens*10)) // 5
+    denomination_one = m-(denomination_tens*10)-(denomination_five*5)
+    return denomination_tens+denomination_five+denomination_one
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
