1a13132e754ec68cee867f977f3d011c3c88c709
amogchandrashekar <amogmc3@gmail.com>
2018-10-22 17:42:27 +0530

    Update primitive_calculator.py

--- Algorithmic Toolbox/week5_dynamic_programming_1/primitive_calculator.py
+++ Algorithmic Toolbox/week5_dynamic_programming_1/primitive_calculator.py
@@ -1,3 +1,41 @@
+"""
+Primitive Calculator
+
+Problem Description
+Task. Given an integer n, compute the minimum number of operations needed to obtain the number n
+starting from the number 1.
+Input Format. The input consists of a single integer 1 ≤ n ≤ 10 6 .
+Output Format. In the first line, output the minimum number k of operations needed to get n from 1.
+In the second line output a sequence of intermediate numbers. That is, the second line should contain
+positive integers a 0 , a 2 , . . . , a k−1 such that a 0 = 1, a k−1 = n and for all 0 ≤ i < k − 1, a i+1 is equal to
+either a i + 1, 2a i , or 3a i . If there are many such sequences, output any one of them.
+
+Sample 1.
+Input:
+1
+Output:
+0
+1
+
+Sample 2.
+Input:
+5
+Output:
+3
+1245
+Here, we first multiply 1 by 2 two times and then add 1. Another possibility is to first multiply by 3
+and then add 1 two times. Hence “1 3 4 5” is also a valid output in this case.
+
+Sample 3.
+Input:
+96234
+Output:
+14
+1 3 9 10 11 22 66 198 594 1782 5346 16038 16039 32078 96234
+Again, another valid output in this case is “1 3 9 10 11 33 99 297 891 2673 8019 16038 16039 48117
+96234”.
+"""
+
 # Uses python3
 import sys
 
@@ -48,9 +86,6 @@ def min_ops(n):
         result[i] = minOperations
 
     return result
-
-
-
 
 input = sys.stdin.read()
 n = int(input)
