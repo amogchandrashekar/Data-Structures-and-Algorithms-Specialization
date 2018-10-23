f2bdd3fef1bf127a2decdf7de7ed20ecea51d427
amogchandrashekar <amogmc3@gmail.com>
2018-10-23 11:47:36 +0530

    Add files via upload

---
+++ Algorithmic Toolbox/week5_dynamic_programming_1/lcs2.py
@@ -0,0 +1,36 @@
+#Uses python3
+
+import sys
+
+def lcs2(a, b):
+    #write your code here
+    m = len(a)
+    n = len(b)
+
+    L = [[None] * (n + 1) for x in range(m + 1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif a[i - 1] == b[j - 1]:
+                L[i][j] = L[i - 1][j - 1] + 1
+            else:
+                L[i][j] = max(L[i - 1][j], L[i][j - 1])
+
+    return L[i][j]
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+
+    n = data[0]
+    data = data[1:]
+    a = data[:n]
+
+    data = data[n:]
+    m = data[0]
+    data = data[1:]
+    b = data[:m]
+
+    print(lcs2(a, b))
