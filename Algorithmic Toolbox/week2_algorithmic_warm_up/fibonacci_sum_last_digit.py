b109a7a6b28e7f9cdf0ab1407f6bb0b9bbcc7dad
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 15:50:44 +0530

    Last Digit of the Sum of Fibonacci Numbers
    Last Digit of the Sum of Fibonacci Numbers

--- Algorithmic Toolbox/week2_algorithmic_warm_up/fibonacci_sum_last_digit.py
+++ Algorithmic Toolbox/week2_algorithmic_warm_up/fibonacci_sum_last_digit.py
@@ -1,3 +1,27 @@
+"""
+
+Problem Description
+
+Task. Given an integer n, find the last digit of the sum F 0 + F 1 + · · · + F n .
+Input Format. The input consists of a single integer n.
+Constraints. 0 ≤ n ≤ 10 18 .
+Output Format. Output the last digit of F 0 + F 1 + · · · + F n .
+
+Sample 1.
+Input:
+3
+Output:
+4
+F 0 + F 1 + F 2 + F 3 = 0 + 1 + 1 + 2 = 4.
+
+Sample 2.
+Input:
+100
+Output:
+5
+The sum is equal to 927 372 692 193 078 999 175, the last digit is 5.
+
+"""
 # Uses python3
 import sys
 
