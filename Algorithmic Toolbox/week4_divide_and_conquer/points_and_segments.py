f0f39eea13710bc968b69cdbf34b4bcf4b34c37d
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 13:14:19 +0530

    Organizing a Lottery
    Organizing a Lottery

--- Algorithmic Toolbox/week4_divide_and_conquer/points_and_segments.py
+++ Algorithmic Toolbox/week4_divide_and_conquer/points_and_segments.py
@@ -1,3 +1,39 @@
+"""
+
+Problem Description
+
+Task. You are given a set of points on a line and a set of segments on a line. The goal is to compute, for
+each point, the number of segments that contain this point.
+
+Input Format. The first line contains two non-negative integers s and p defining the number of segments
+and the number of points on a line, respectively. The next s lines contain two integers a i , b i defining
+the i-th segment [a i , b i ]. The next line contains p integers defining points x 1 , x 2 , . . . , x p .
+Constraints. 1 ≤ s, p ≤ 50000; −10 8 ≤ a i ≤ b i ≤ 10 8 for all 0 ≤ i < s; −10 8 ≤ x j ≤ 10 8 for all 0 ≤ j < p.
+Output Format. Output p non-negative integers k 0 , k 1 , . . . , k p−1 where k i is the number of segments which
+contain x i . More formally,
+k i = |{j : a j ≤ x i ≤ b j }| .
+
+Sample 1.
+Input:
+23
+05
+7 10
+1 6 11
+Output:
+100
+Here, we have two segments and three points. The first point lies only in the first segment while the
+remaining two points are outside of all the given segments.
+
+Sample 2.
+Input:
+13
+-10 10
+-100 100 0
+Output:
+001
+
+"""
+
 # Uses python3
 import sys
 from itertools import chain
