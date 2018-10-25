a503dfec760b52d4efb8b7b17914ec8d9e59d451
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 15:46:08 +0530

    Least Common Multiple
    Least Common Multiple

--- Algorithmic Toolbox/week2_algorithmic_warm_up/lcm.py
+++ Algorithmic Toolbox/week2_algorithmic_warm_up/lcm.py
@@ -1,3 +1,28 @@
+"""
+
+Problem Description
+Task. Given two integers a and b, find their least common multiple.
+Input Format. The two integers a and b are given in the same line separated by space.
+Constraints. 1 ≤ a, b ≤ 2 · 10 9 .
+Output Format. Output the least common multiple of a and b.
+
+Sample 1.
+Input:
+68
+Output:
+24
+Among all the positive integers that are divisible by both 6 and 8 (e.g., 48, 480, 24), 24 is the smallest
+one.
+
+Sample 2.
+Input:
+28851538 1183019
+Output:
+1933053046
+1933053046 is the smallest positive integer divisible by both 28851538 and 1183019.
+
+"""
+
 # Uses python3
 import sys
 
