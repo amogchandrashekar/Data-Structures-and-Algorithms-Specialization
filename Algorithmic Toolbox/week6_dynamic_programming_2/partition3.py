593ef0136fabae73db9ac5f694ca31fb93555646
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 12:35:39 +0530

    Update partition3.py

--- Algorithmic Toolbox/week6_dynamic_programming_2/partition3.py
+++ Algorithmic Toolbox/week6_dynamic_programming_2/partition3.py
@@ -1,6 +1,46 @@
+"""
+Problem Description
+
+Input Format. The first line contains an integer n. The second line contains integers v 1 , v 2 , . . . , v n separated
+by spaces.
+Constraints. 1 ≤ n ≤ 20, 1 ≤ v i ≤ 30 for all i.
+Output Format. Output 1, if it possible to partition v 1 , v 2 , . . . , v n into three subsets with equal sums, and
+0 otherwise.
+
+Sample 1.
+Input:
+4
+3333
+Output:
+0
+
+Sample 2.
+Input:
+1
+40
+Output:
+0
+
+Sample 3.
+Input:
+11
+17 59 34 57 17 23 67 1 18 2 59
+Output:
+1
+34 + 67 + 17 = 23 + 59 + 1 + 17 + 18 = 59 + 2 + 57.
+
+Sample 4.
+Input:
+13
+1 2 3 4 5 5 7 7 8 10 12 19 25
+Output:
+1
+1 + 3 + 7 + 25 = 2 + 4 + 5 + 7 + 8 + 10 = 5 + 12 + 19.
+
+"""
 # Uses python3
 import sys
-import itertools
+
 
 def subsetsum(A,n,sum_1,sum_2,sum_3,lookup):
 
@@ -51,4 +91,3 @@ if __name__ == '__main__':
     n, *A = list(map(int, input.split()))
     # A = [3,3,3]
     print(partition3(A))
-
