6e499991408e841f1bb5cc97eb15b377d58309f4
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 16:03:47 +0530

    Money Change
    Money Change

--- Algorithmic Toolbox/week3_greedy_algorithms/change.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/change.py
@@ -1,3 +1,29 @@
+"""
+
+Problem Description
+Task. The goal in this problem is to find the minimum number of coins needed to change the input value
+(an integer) into coins with denominations 1, 5, and 10.
+Input Format. The input consists of a single integer m.
+Constraints. 1 ≤ m ≤ 10 3 .
+Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes m.
+
+Sample 1.
+Input:
+2
+Output:
+2
+2 = 1 + 1.
+
+Sample 2.
+Input:
+28
+Output:
+6
+28 = 10 + 10 + 5 + 1 + 1 + 1.
+
+"""
+
+
 import sys
 
 def get_change(m):
