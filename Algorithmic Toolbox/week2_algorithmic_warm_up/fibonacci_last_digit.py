930d40a4a9525bb9e402096936de371f0451de0e
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 15:42:32 +0530

    Last Digit of a Large Fibonacci Number
    Last Digit of a Large Fibonacci Number

--- Algorithmic Toolbox/week2_algorithmic_warm_up/fibonacci_last_digit.py
+++ Algorithmic Toolbox/week2_algorithmic_warm_up/fibonacci_last_digit.py
@@ -1,3 +1,26 @@
+"""
+Problem Description
+
+Task. Given an integer n, find the last digit of the nth Fibonacci number F n (that is, F n mod 10).
+Input Format. The input consists of a single integer n.
+Constraints. 0 ≤ n ≤ 10 7 .
+Output Format. Output the last digit of F n .
+
+Sample 1.
+Input:
+3
+Output:
+2
+F 3 = 2.
+
+Sample 2.
+Input:
+331
+Output:
+9
+F 331 = 668 996 615 388 005 031 531 000 081 241 745 415 306 766 517 246 774 551 964 595 292 186 469
+
+"""
 # Uses python3
 import sys
 
