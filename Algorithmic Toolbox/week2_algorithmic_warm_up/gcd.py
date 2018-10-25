984e4a04559a968094020cf5f7cc9602f67d007b
amogchandrashekar <amogmc3@gmail.com>
2018-10-25 15:43:41 +0530

    Greatest Common Divisor
    Greatest Common Divisor

--- Algorithmic Toolbox/week2_algorithmic_warm_up/gcd.py
+++ Algorithmic Toolbox/week2_algorithmic_warm_up/gcd.py
@@ -1,3 +1,26 @@
+"""
+Problem Description
+
+Task. Given two integers a and b, find their greatest common divisor.
+Input Format. The two integers a, b are given in the same line separated by space.
+Constraints. 1 ≤ a, b ≤ 2 · 10 9 .
+Output Format. Output GCD(a, b).
+
+Sample 1.
+Input:
+18 35
+Output:
+1
+18 and 35 do not have common non-trivial divisors.
+
+Sample 2.
+Input:
+28851538 1183019
+Output:
+17657
+28851538 = 17657 · 1634, 1183019 = 17657 · 67.
+
+"""
 # Uses python3
 import sys
 
