331abf487dd33d624c4d463b0083667b2dbb5201
Amog Chandrashekar <amogmc3@gmail.com>
2020-04-14 08:27:35 +0530

    Implement trie matching algorithm

---
+++ Algorithms on Strings/week1_suffix_trees/trie_matching.py
@@ -0,0 +1,91 @@
+# python3
+
+"""
+Task.
+	Implement TrieMatching algorithm.
+
+Input Format.
+ 	The first line of the input contains a string Text, the second line contains an integer n,
+	each of the following n lines contains a pattern from Patterns = {p 1 , . . . , p n }.
+	Constraints. 1 ≤ |Text| ≤ 10 000; 1 ≤ n ≤ 5 000; 1 ≤ |p i | ≤ 100 for all 1 ≤ i ≤ n; all strings contain only
+	symbols A, C, G, T; no p i is a prefix of p j for all 1 ≤ i ̸ = j ≤ n.
+
+Output Format.
+ 	All starting positions in Text where a string from Patterns appears as a substring in
+	increasing order (assuming that Text is a 0-based array of symbols).
+
+Sample 1.
+Input:
+	AAA
+	1
+	AA
+Output:
+	01
+Explanation:
+	The pattern AA appears at positions 0 and 1. Note that these two occurrences of the pattern overlap.
+"""
+
+import sys
+from collections import defaultdict
+
+
+def create_trie(patterns):
+	"""
+	Create trie from the patterns
+	"""
+	trie = defaultdict(dict)
+	counter = 0
+	for pattern in patterns:
+		current_node = 0
+		for letter in pattern:
+			if letter in trie[current_node]:
+				current_node = trie[current_node][letter]
+			else:
+				counter += 1
+				trie[current_node][letter] = counter
+				current_node = counter
+	return trie
+
+
+def solve(text, patterns):
+	"""
+	Create trie, check if text spans across the trie and find matches
+	"""
+	result = list()
+	trie = create_trie(patterns)
+
+	def prefix_trie_matching(text):
+		index, counter_index = 0, 0
+		symbol = text[index]
+		current_branch = trie[0]
+		while True:
+			if not current_branch:
+				return True # Comes here when the pattern is read due to defaultdict
+			elif symbol in current_branch:
+				counter_index = trie[counter_index][symbol]
+				current_branch = trie[counter_index]
+				index += 1
+				if index < len(text):
+					symbol = text[index]
+				else:
+					symbol = "$" # if length of text < length of substring, it should return false
+			else:
+				return False
+
+	for index in range(len(text)):
+		if prefix_trie_matching(text[index:]):
+			result.append(index)
+
+	return result
+
+
+if __name__ == "__main__":
+	text = sys.stdin.readline ().strip ()
+	n = int (sys.stdin.readline ().strip ())
+	patterns = []
+	for i in range (n):
+		patterns += [sys.stdin.readline ().strip ()]
+	# text = "A"
+	# patterns = ["AA"]
+	ans = solve(text, patterns)
+	sys.stdout.write (' '.join (map (str, ans)) + '\n')
